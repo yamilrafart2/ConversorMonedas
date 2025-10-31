d20b827a55025a2a17eb445890ef6611e3390c0c
yamilrafart2 <yamilrafart2>
2025-10-30 21:15:32 -0300

    Add menú para las funcionalidades del conversor

---
+++ src/com/conversor/principal/App.java
@@ -0,0 +1,102 @@
+package com.conversor.principal;
+
+import com.conversor.calculos.ConversorDeMonedas;
+import com.conversor.modelos.Conversion;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner lectura = new Scanner(System.in);
+        ConversorDeMonedas consulta = new ConversorDeMonedas();
+
+        int opcion = -1;
+        while (opcion != 7) {
+            exibirMenu();
+            try {
+                opcion = Integer.valueOf(lectura.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Por favor, ingrese un número válido (1-7).");
+                continue; // Vuelve al inicio del loop
+            }
+
+            if (opcion > 0 && opcion < 7) {
+
+                // --- 1. PEDIR EL MONTO ---
+                System.out.println("Ingrese el valor que desear convertir: ");
+                double monto;
+                try {
+                    monto = Double.valueOf(lectura.nextLine());
+                } catch (NumberFormatException e) {
+                    System.out.println("Error: Monto no válido. Intente de nuevo.");
+                    continue; // Vuelve al inicio del loop
+                }
+
+                // --- 2. DEFINIR MONEDAS SEGÚN OPCIÓN ---
+                String monedaOrigen = "";
+                String monedaDestino = "";
+
+                switch (opcion) {
+                    case 1:
+                        monedaOrigen = "USD";
+                        monedaDestino = "ARS";
+                        break;
+                    case 2:
+                        monedaOrigen = "ARS";
+                        monedaDestino = "USD";
+                        break;
+                    case 3:
+                        monedaOrigen = "USD";
+                        monedaDestino = "BRL";
+                        break;
+                    case 4:
+                        monedaOrigen = "BRL";
+                        monedaDestino = "USD";
+                        break;
+                    case 5:
+                        monedaOrigen = "USD";
+                        monedaDestino = "COP";
+                        break;
+                    case 6:
+                        monedaOrigen = "COP";
+                        monedaDestino = "USD";
+                        break;
+                }
+
+                // --- 3. EJECUTAR CONSULTA Y MOSTRAR RESULTADO ---
+                Conversion conversion = consulta.convierteMonedas(monedaOrigen, monedaDestino);
+
+                // --- 4. CALCULAR Y MOSTRAR
+                double resultado = monto * conversion.conversion_rate();
+                System.out.println("El valor " + monto + " [" + monedaOrigen + "] corresponde al valor final de =>>> " + resultado + " [" + monedaDestino + "]\n");
+
+            } else if (opcion != 7) {
+                System.out.println("Opción no válida. Elija entre 1 y 7.");
+            }
+        }
+
+        System.out.println("Finalizando la aplicación... ¡Gracias por usar el Conversor de Moneda!");
+        lectura.close();
+
+    }
+
+    public static void exibirMenu() {
+        System.out.println("""
+                *******************************************************
+                Sea bienvenid@ al Conversor de Moneda =]
+                
+                1) Dólar =>> Peso argentino
+                2) Peso argentino =>> Dólar
+                3) Dólar =>> Real brasileño
+                4) Real brasileño =>> Dólar
+                5) Dólar ==> Peso colombiano
+                6) Peso colombiano ==> Dólar
+                7) Salir
+                Elija una opción válida:
+                *******************************************************
+                """);
+    }
+
+}
